5b9b02b87c18ce913ed1af7259afb2a33237722a
Enadabuzaid <enad.abuzaid15@gmail.com>
2022-01-29 14:58:26 +0200

    add elzero folder to files

---
+++ Elzero/js/main-scripts.js
@@ -0,0 +1,13 @@
+// TERNARY OPERATOR
+
+// console.log('**************************');
+// console.log(' *** ternary Examples ****\n\n\n');
+
+// const age = 45;
+
+// age <= 10 ? console.log(`the age ${age} is less than 10`) 
+// : age >= 10 && age < 20 ?  console.log(`the age ${age} is between 10 and 20 `)
+// : age >= 20 && age < 60 ? console.log(`the age ${age} is between 20 and 60`)
+// : age >= 60 ? console.log(`the age ${age} is greater than 20`)
+// : console.log(' UNDEFIEND age please enter age');  
+// console.log('\n**************************');
