634c6d1ecce43cd29b96a0b558c26352bac4fac4
Enadabuzaid <enad.abuzaid15@gmail.com>
2022-03-05 18:20:14 +0300

    add functions assgiments

--- Elzero/js/assginmets.js
+++ Elzero/js/assginmets.js
@@ -349,4 +349,233 @@ console.log('*** [5] *** ');
 // "5 => Sameh"
 console.log("DONE");
 
+console.log('\n\n\n\n************* assignment 57-63');
+console.log('*** [1] *** ');
+function sayHello(theName, theGender = "") {
+  if(theGender == "Male"){
+      txt = "Mr";
+  } else if(theGender == "Female"){
+    txt = "Miss";
+  } else {
+      txt = "";
+  }
+  console.log(`Hello ${txt} ${theName}`);
+}
 
+// Needed Output
+sayHello("Osama", "Male"); // "Hello Mr Osama"
+sayHello("Eman", "Female"); // "Hello Miss Eman"
+sayHello("Sameh"); // "Hello Sameh"
+
+console.log('*** [2] *** ');
+
+function calculate(firstNum, secondNum, operation) {
+  if(typeof secondNum == 'undefined'){
+    console.log(`Second Number Not Found`);
+  } else {
+
+    switch (operation){
+      case "add":
+        console.log(firstNum + secondNum); 
+        break; 
+      
+      case "subtract":
+        console.log(firstNum - secondNum);  
+        break;
+
+      case "multiply" :
+        console.log(firstNum * secondNum);  
+        break;
+
+      default:
+        console.log(firstNum + secondNum); 
+    }
+  }   
+}
+
+// Needed Output
+calculate(20); // Second Number Not Found
+calculate(20, 30); // 50
+calculate(20, 30, 'add'); // 50
+calculate(20, 30, 'subtract'); // -10
+calculate(20, 30, 'multiply'); // 600
+
+console.log('*** [3] *** ');
+function ageInTime(theAge) {
+  if(theAge >= 10 && theAge <= 100){
+    let result = [];
+
+    let month = theAge * 12;
+    let week = month * 4;
+    let day = week * 7;
+    let hour = day * 24;
+    let min = hour * 60;
+    let sec = min  * 60;
+
+
+    console.log(`${month} months`);
+    console.log(`${week} weeks`);
+    console.log(`${day} days`);
+    console.log(`${hour} hours`);
+    console.log(`${min} min`);
+    console.log(`${sec} sec`);
+
+
+
+    
+
+  } else{
+    console.log(`Age Out Of Range`);
+  }
+}
+
+// Needed Output
+ageInTime(110); // Age Out Of Range
+ageInTime(38); // Months Example => 456 Months
+
+console.log('*** [4] *** ');
+function checkStatus(a, b, c) {
+  // a 
+
+  if(typeof(a) == "string"){
+    fname = a;
+
+  } else if (typeof(a) == "number"){
+    numberfun = a;
+
+  } else if (typeof(a) == "boolean") {
+    hireStatus = a;
+  }
+
+  if(typeof(b) == "string"){
+    fname = b;
+  } else if (typeof(b) == "number"){
+    numberfun = b;
+  } else if (typeof(b) == "boolean") {
+    hireStatus = b;
+  }
+
+    if(typeof(c) == "string"){
+    fname = c;
+  } else if (typeof(c) == "number"){
+    numberfun = b;
+  } else if (typeof(c) == "boolean") {
+    hireStatus = c;
+  }
+
+
+  if(hireStatus){
+    hire  = "Available";
+  } else{
+    hire = "Not Available";
+  }
+
+
+  console.log(`Hello ${fname} , Your Age is ${numberfun}, You Are ${hire} For Hire.`);
+
+}
+
+// Needed Output
+checkStatus("Osama", 38, true); // "Hello Osama, Your Age Is 38, You Are Available For Hire"
+checkStatus(38, "Osama", true); // "Hello Osama, Your Age Is 38, You Are Available For Hire"
+checkStatus(true, 38, "Osama"); // "Hello Osama, Your Age Is 38, You Are Available For Hire"
+checkStatus(false, "Osama", 38); // "Hello Osama, Your Age Is 38, You Are Not Available For Hire"
+checkStatus(false, 38,"Osama");
+
+console.log('*** [5] *** ');
+
+document.write(`<h3>This ass 5 in function from 57 to 63</h3>`)
+
+function createSelectBox(startYear, endYear) {
+  document.write(`<select>`);
+    for(let i = startYear; i <= endYear; i++  ){
+      document.write(`<option value="${i}">${i}</option>`);
+    }
+  document.write(`</select>`);
+}
+createSelectBox(2000, 2021);
+
+console.log('*** [6] *** ');
+function multiply(...numbers){
+  let result =1 ;
+  for(let i = 0; i < numbers.length; i++){
+    
+    if(typeof(numbers[i]) != "string"){
+      
+      result *= parseInt(numbers[i]);
+
+    }
+
+  }
+  
+  console.log(result);
+}
+
+multiply(10, 20); // 200
+multiply("A", 10, 30); // 300
+multiply(100.5, 10, "B"); // 1000
+
+console.log('\n\n\n\n************* assignment 64-70');
+console.log('*** [1] *** ');
+function getDetails(zName, zAge, zCountry) {
+  
+    
+  function namePattern(zName) {
+    // Write Your Code Here
+    // Osama Mohamed => Osama M.
+    // Ahmed ali => Ahmed A.
+
+      nameSplit = zName.split(" ");
+      firstName  = nameSplit[0];
+      secondName = `${nameSplit[1][0].toUpperCase()}.`;
+
+      fullName = firstName + ' ' + secondName;
+      return fullName;
+
+  }
+  function ageWithMessage(zAge) {
+    // Write Your Code Here
+    // 38 Is My Age => Your Age Is 38
+    // 32 Is The Age => Your Age Is 32
+    age =zAge.substring(0, 3)
+    return `Your Age Is ${age}`;
+  }
+  function countryTwoLetters(zCountry) {
+    // Write Your Code Here
+    // Egypt => You Live In EG
+    // Syria => You Live In SY
+    country = zCountry.slice(0, 2);
+    return country.toUpperCase();
+  }
+  function fullDetails() {
+    // Write Your Code Here
+    
+    return `Hello ${ namePattern(zName)} , ${ageWithMessage(zAge)} , ${countryTwoLetters(zCountry)}`;
+    
+  }
+  return fullDetails(); // Do Not Edit This
+}
+
+console.log(getDetails("Osama Mohamed", "38 Is My Age", "Egypt"));
+// Hello Osama M., Your Age Is 38, You Live In EG
+
+console.log(getDetails("Ahmed ali", "32 Is The Age", "Syria"));
+// Hello Ahmed A., Your Age Is 32, You Live In SY
+
+console.log(getDetails("Enad Abuzaid", "24 Is The Age", "Jordan"));
+
+
+console.log('*** [2] *** ');
+// function itsMe() {
+//   return `Iam A Normal Function`;
+// }
+
+// console.log(itsMe()); // Iam A Normal Function
+
+// let arrow = () => `I am An Arrow function`;
+
+// console.log(arrow);
+
+
+console.log('*** [3] *** ');
+console.log('*** [4] *** ');
